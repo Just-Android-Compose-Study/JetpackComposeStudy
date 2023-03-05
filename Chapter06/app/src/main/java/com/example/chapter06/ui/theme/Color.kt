b01b8f1e15aa041133076b7312fee6d218a3fce3
boring-km <kmjin@wjtb.net>
2023-03-05 19:46:26 +0900

    research/#13: 6장 예제코드 Material3로 다시 고쳐보기

--- Chapter06/app/src/main/java/com/example/chapter06/ui/theme/Color.kt
+++ Chapter06/app/src/main/java/com/example/chapter06/ui/theme/Color.kt
@@ -2,10 +2,7 @@ package com.example.chapter06.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val AndroidGreen = Color(0xFF3DDC84)
+val AndroidGreenDark = Color(0xFF20B261)
+val Orange = Color(0xFFFFA500)
+val OrangeDark = Color(0xFFCC8400)
