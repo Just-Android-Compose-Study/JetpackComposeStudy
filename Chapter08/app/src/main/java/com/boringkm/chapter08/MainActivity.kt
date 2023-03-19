b7477df307a597cf59513e8e41e2a170ae245a47
boring-km <kms0644804@naver.com>
2023-03-19 09:24:22 +0900

    research/#17: 8장 프로젝트 초기화

---
+++ Chapter08/app/src/main/java/com/boringkm/chapter08/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.boringkm.chapter08
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.boringkm.chapter08.ui.theme.Chapter08Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Chapter08Theme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    Chapter08Theme {
+        Greeting("Android")
+    }
+}
