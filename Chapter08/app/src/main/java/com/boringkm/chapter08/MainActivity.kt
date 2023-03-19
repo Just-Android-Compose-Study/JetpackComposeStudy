36b540b62086ef7773148e846eb41b8a5fd22c2d
boring-km <kms0644804@naver.com>
2023-03-19 10:52:17 +0900

    research/#17: 8장 애니메이션 적용 정리

--- Chapter08/app/src/main/java/com/boringkm/chapter08/MainActivity.kt
+++ Chapter08/app/src/main/java/com/boringkm/chapter08/MainActivity.kt
@@ -3,13 +3,21 @@ package com.boringkm.chapter08
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.animation.*
+import androidx.compose.animation.core.*
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.boringkm.chapter08.ui.theme.Chapter08Theme
 
 class MainActivity : ComponentActivity() {
@@ -22,22 +30,261 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    StateChangeDemo()
                 }
             }
         }
     }
 }
 
+@Preview
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun StateChangeDemo() {
+    var toggled by remember {
+        mutableStateOf(false)
+    }
+    val color by animateColorAsState(
+        targetValue = if (toggled)
+            Color.Blue
+        else
+            Color.Red,
+        animationSpec = tween(durationMillis = 500)
+    )
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Button(onClick = {
+            toggled = !toggled
+        }) {
+            Text(
+                stringResource(R.string.toggle)
+            )
+        }
+        Box(
+            modifier = Modifier
+                .padding(top = 32.dp)
+                .background(color = color)
+                .size(128.dp)
+        )
+    }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
-    Chapter08Theme {
-        Greeting("Android")
+@Preview
+fun MultipleValuesAnimationDemo() {
+    var toggled by remember {
+        mutableStateOf(false)
+    }
+    val transition = updateTransition(
+        targetState = toggled,  // 이 상태값이 변하면 transition 작동
+        label = "toggledTransition"
+    )
+    val borderWidth by transition.animateDp(label = "borderWidthTransition") { state ->
+        if (state)
+            10.dp
+        else
+            1.dp
+    }
+
+    // 상태 값에 따라 animate
+    val degrees by transition.animateFloat(label = "degreesTransition") { state ->
+        if (state) -360F
+        else
+            0F
+    }
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Button(onClick = {
+            toggled = !toggled  // 상태 변경
+        }) {
+            Text(
+                stringResource(R.string.toggle)
+            )
+        }
+        Box(
+            contentAlignment = Alignment.Center,
+            modifier = Modifier
+                .padding(top = 32.dp)
+                .border(
+                    width = borderWidth,
+                    color = Color.Black
+                )
+                .size(128.dp)
+        ) {
+            Text(
+                text = stringResource(id = R.string.app_name),
+                modifier = Modifier.rotate(degrees = degrees)
+            )
+        }
+    }
+}
+
+@Composable
+@Preview
+fun AnimatedVisibilityDemo() {
+    var visible by remember {
+        mutableStateOf(false)
+    }
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Button(onClick = {
+            visible = !visible
+        }) {
+            Text(
+                stringResource(
+                    id = if (visible)
+                        R.string.hide
+                    else
+                        R.string.show
+                )
+            )
+        }
+        AnimatedVisibility(
+            visible = visible,
+            enter = slideInHorizontally(initialOffsetX = { -it }),
+            exit = fadeOut(animationSpec = tween(durationMillis = 300))
+        ) {
+            Box(
+                modifier = Modifier
+                    .padding(top = 32.dp)
+                    .background(color = Color.Red)
+                    .size(128.dp)
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+fun SizeChangeAnimationDemo() {
+    var size by remember { mutableStateOf(1F) }
+
+    val transition = updateTransition(
+        targetState = size,  // 이 상태값이 변하면 transition 작동
+        label = "sizeTransition"
+    )
+
+    val fontSize by transition.animateFloat(label = "text") { state ->
+        state
+    }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    ) {
+        Slider(
+            value = size,
+            valueRange = (1F..4F),
+            steps = 3,
+            onValueChange = {
+                size = it
+            },
+            modifier = Modifier.padding(bottom = 8.dp)
+        )
+        Text(
+            text = stringResource(id = R.string.lines),
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(Color.White)
+                .animateContentSize(tween(durationMillis = 300)),
+            maxLines = fontSize.toInt(), color = Color.Blue
+        )
+        Text(
+            text = stringResource(id = R.string.app_name),
+            fontSize = (fontSize * 8).sp
+        )
+    }
+}
+
+@Preview
+@Composable
+fun CrossfadeAnimationDemo() {
+    var isFirstScreen by remember { mutableStateOf(true) }
+    Column(
+        modifier = Modifier
+            .fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Switch(
+            checked = isFirstScreen,
+            onCheckedChange = {
+                isFirstScreen = !isFirstScreen
+            },
+            modifier = Modifier.padding(top = 16.dp, bottom = 16.dp)
+        )
+        Crossfade(
+            targetState = isFirstScreen,
+            animationSpec = spring(stiffness = Spring.StiffnessVeryLow)
+        ) {
+            if (it) {
+                Screen(
+                    text = stringResource(id = R.string.letter_w),
+                    backgroundColor = Color.Gray
+                )
+            } else {
+                Screen(
+                    text = stringResource(id = R.string.letter_i),
+                    backgroundColor = Color.LightGray
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun Screen(
+    text: String,
+    backgroundColor: Color = Color.White
+) {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = backgroundColor),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            text = text,
+            style = MaterialTheme.typography.displayLarge
+        )
+    }
+}
+
+@Composable
+@Preview
+fun InfiniteRepeatableDemo() {
+    val infiniteTransition = rememberInfiniteTransition()
+    val degrees by infiniteTransition.animateFloat(
+        initialValue = 0F,
+        targetValue = 0F,
+        animationSpec = infiniteRepeatable(animation = keyframes {
+            durationMillis = 3000
+            0F at 0
+            180F at 750
+            359F at 750 + 750
+            180F at 750 + 750 + 750
+            0F at 750 + 750 + 750 + 750
+        })
+    )
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            text = stringResource(id = R.string.app_name),
+            modifier = Modifier.rotate(degrees = degrees),
+            fontSize = (degrees / 6).sp
+        )
     }
 }
